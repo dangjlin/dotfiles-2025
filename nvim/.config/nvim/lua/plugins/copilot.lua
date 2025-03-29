880cf217f42883319ffc9eb33e44837ee0017e27
Daniel Lin <daniel.lin@abagile.com>
2025-03-30 01:04:03 +0800

    Make the copilot keymap easier to use
    Original tab has been taken by lsp autocomplete used words.
    
    and the show next and previous suggestion didn't work by default
    and thus I remap it to arrow keys

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -2,7 +2,20 @@ return {
   "github/copilot.vim",
   event = "InsertEnter", -- Load Copilot only when entering Insert mode
   config = function()
-    print("Copilot plugin loaded!") -- Debugging print
+    print("Copilot plugin start!") -- Debugging print
     vim.cmd("Copilot enable")
+    -- Remap accept tab to ctrl + j because auto complete words has been set to tab
+    vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false
+    })
+    vim.g.copilot_no_tab_map = true
+    -- Remap `<Right>` to go to the next Copilot suggestion
+    vim.api.nvim_set_keymap("i", "<Right>", "<Plug>(copilot-next)", { silent = true, noremap = false })
+    -- Optional: Use `<Left>` to cycle backward
+    vim.api.nvim_set_keymap("i", "<Left>", "<Plug>(copilot-previous)", { silent = true, noremap = false })
+
+    vim.cmd("Copilot status")
+    print("Copilot plugin loaded!") -- Debugging print
   end
 }
